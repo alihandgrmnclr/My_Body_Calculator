61a80398e9b91f29d949808e09f0a4c3a7662f63
Alihan Degirmenciler <alihandegirmenciler@gmail.com>
2022-09-06 21:10:07 +0300

    updates

--- body/src/App.js
+++ body/src/App.js
@@ -1,6 +1,8 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
 import { useState } from "react"
+import { Formik, useFormik } from "formik"
+import Validation from './components/Validation';
 
 function App() {
 
@@ -12,16 +14,69 @@ function App() {
   const [height, setHeight] = useState("");
   const [hip, setHip] = useState("");
   const [bodyFat, setBodyFat] = useState("");
+  const [weight, setWeight] = useState("");
+  const [bmi, setBMI] = useState("");
+  const [status, setStatus] = useState("");
 
   const CalcMan = () => {
     const BodyFat = (495 / (1.0324 - 0.19077 * Math.log10((+waist) - (+neck)) + 0.15456 * Math.log10(height)) - 450).toFixed(2);  // number'a çevirmek için "+" koyduk (veri string olarak geliyor)
     setBodyFat(BodyFat);
+    if (BodyFat>6 && BodyFat<13) {            // Setting your status (average, obesite exc..)
+      setStatus("You are athletic");
+    } else if (BodyFat>13 && BodyFat<17) {
+      setStatus("You are in shape");
+    } else if (BodyFat>17 && BodyFat<25){
+      setStatus("You are in average");
+    } else if (BodyFat>25){
+      setStatus("You are obesite");
+    }
   };
 
   const CalcWoman = () => {
     const BodyFat = (495 / (1.29579 - 0.35004 * Math.log10((+waist) + (+hip) - (+neck)) + 0.22100 * Math.log10(height)) - 450).toFixed(2);  // number'a çevirmek için "+" koyduk (veri string olarak geliyor)
     setBodyFat(BodyFat);
+    if (BodyFat>10 && BodyFat<13) {            // Setting your status (average, obesite exc..)
+      setStatus("You are essential");
+    } else if (BodyFat>13 && BodyFat<20) {
+      setStatus("You are athletic");
+    } else if (BodyFat>20 && BodyFat<25){
+      setStatus("You are in shape");
+    } else if (BodyFat>25 && BodyFat<32){
+      setStatus("You are average");
+    }else if (BodyFat>32){
+      setStatus("You are obesite");
+    }
   };
+
+  const CalcBMI = () => {
+    setBMI(((+weight) / ((height / 100) ** 2)).toFixed(2));
+  };
+
+
+
+  //formik
+  const { handleSubmit, handleChange, values, errors, touched, handleBlur } = useFormik({
+    initialValues: {
+      height: "",
+      neck: "",
+      waist: "",
+      weight: "",
+      hip: ""
+    },
+    onSubmit: (values) => {
+      console.log(height, neck, weight, waist, hip);
+      setHeight(values.height);
+      setNeck(values.neck);
+      setWaist(values.waist);
+      setWeight(values.weight);
+      setHip(values.hip);
+      CalcMan();
+      CalcBMI();
+    },
+    validationSchema: Validation
+  });
+
+
 
   return (
     <div className="App">
@@ -44,7 +99,29 @@ function App() {
       {gender ? (gender === "male" ?
         <div className="container">
           <div className="row">
-            <div className="col-sm-12 justify-content-center">
+            <div className="col-sm-12 justify-content-center myForm">
+              {/* <form onSubmit={handleSubmit}>
+                <label htmlFor="height">Your Height</label>
+                <input type="number" name='height' value={values.height} onChange={handleChange} onBlur={handleBlur} placeholder="175" />
+                {errors.height && touched.height && (<div className='error'>{errors.height}</div>)} <br />
+
+                <label htmlFor="neck">Your Neck</label>
+                <input type="number" name='neck' value={values.neck} onChange={handleChange} onBlur={handleBlur} placeholder="35" />
+                {errors.neck && touched.neck && (<div className='error'>{errors.neck}</div>)}<br />
+
+                <label htmlFor="waist">Your Waist</label>
+                <input type="number" name='waist' value={values.waist} onChange={handleChange} onBlur={handleBlur} placeholder="85" />
+                {errors.waist && touched.waist && (<div className='error'>{errors.waist}</div>)} <br />
+
+                <label htmlFor="weight">Your Weight</label>
+                <input type="number" name='weight' value={values.weight} onChange={handleChange} onBlur={handleBlur} placeholder="75" />
+                {errors.weight && touched.weight && (<div className='error'>{errors.weight}</div>)} <br />
+                <br /><br />
+
+                <button type='submit' className='btn btn-primary' >Submit</button>
+
+              </form>
+              {bodyFat && <div className='mt-3'>Your Body fat is {bodyFat}% <br /><div>Your BMI is: {bmi}</div></div>} */}
               <div>
                 <span>Your Height </span>
                 <input placeholder='175' type="number" value={height} onChange={(e) => setHeight(e.target.value)} /> </div>
@@ -56,33 +133,43 @@ function App() {
                 <span>Your waist circumference (the thinnest)</span>
                 <input placeholder='85' type="number" value={waist} onChange={(e) => setWaist(e.target.value)} />
               </div>
-              <button className="btn btn-primary mt-3" onClick={() => CalcMan()}>Calculate</button>
-              {bodyFat && <div className='mt-3'>Your Body fat is {bodyFat}%</div>}
+              <div>
+                <span>Your weight</span>
+                <input placeholder='85' type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+              </div>
+              <button className="btn btn-primary mt-3" onClick={() => { CalcMan(); CalcBMI(); }}>Calculate</button> <br />
+              {bodyFat && <div className='mt-3'>Your Body fat is {bodyFat}% <br /><div>Your BMI is: {bmi} <br /><div>{status}</div></div></div>}
 
             </div>
           </div>
         </div>
         :
-        <div>
-          <div className="col-sm-12 justify-content-center">
-            <div>
-              <span>Your Height </span>
-              <input placeholder='175' type="number" value={height} onChange={(e) => setHeight(e.target.value)} /> </div>
-            <div>
-              <span>Your neck width (the widest)</span>
-              <input placeholder='35' type="number" value={neck} onChange={(e) => setNeck(e.target.value)} />
-            </div>
-            <div>
-              <span>Your waist circumference (the thinnest)</span>
-              <input placeholder='85' type="number" value={waist} onChange={(e) => setWaist(e.target.value)} />
-            </div>
-            <div>
-              <span>Your hip circumference (the widest)</span>
-              <input placeholder='95' type="number" value={hip} onChange={(e) => setHip(e.target.value)} />
-            </div>
-            <button className="btn btn-primary mt-3" onClick={() => CalcWoman()}>Calculate</button>
-            {bodyFat && <div className='mt-3'>Your Body fat is {bodyFat}%</div>}
+        <div className='container'>
+          <div className="row">
+            <div className="col-sm-12 justify-content-center myForm">
+              <div>
+                <span>Your Height </span>
+                <input placeholder='175' type="number" value={height} onChange={(e) => setHeight(e.target.value)} /> </div>
+              <div>
+                <span>Your neck width (the widest)</span>
+                <input placeholder='35' type="number" value={neck} onChange={(e) => setNeck(e.target.value)} />
+              </div>
+              <div>
+                <span>Your waist circumference (the thinnest)</span>
+                <input placeholder='85' type="number" value={waist} onChange={(e) => setWaist(e.target.value)} />
+              </div>
+              <div>
+                <span>Your hip circumference (the widest)</span>
+                <input placeholder='95' type="number" value={hip} onChange={(e) => setHip(e.target.value)} />
+              </div>
+              <div>
+                <span>Your weight</span>
+                <input placeholder='85' type="number" value={weight} onChange={(e) => setWeight(e.target.value)} />
+              </div>
+              <button className="btn btn-primary mt-3" onClick={() => { CalcWoman(); CalcBMI(); }}>Calculate</button> <br />
+              {bodyFat && <div className='mt-3'>Your Body fat is {bodyFat}% <br /><div>Your BMI is: {bmi}<br/><div>{status}</div></div></div>}
 
+            </div>
           </div>
         </div>)
         : <div>{/* gender yoksa boş div */}</div>
---
+++ body/src/components/Validation.js
@@ -0,0 +1,6 @@
+import { object, string, number, date, InferType, ref } from 'yup';
+
+const Validation = object({
+    
+});
+export default Validation;
